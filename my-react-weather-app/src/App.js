ed0fad8d2ef50184da2609d11f637d8d86dee50c
Sibongile <sibongile.nhlema@gmail.com>
2023-11-26 15:09:27 +0200

    Changed css

--- my-react-weather-app/src/App.js
+++ my-react-weather-app/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>The Awesome Weather App</header>
+      <header  >The Awesome Weather App</header>
       <section className= "weather-app">
         <NearbyCities />
         <Weather defaultCity="Durban"/>
